8196533ffcfafb0fc98e7d20b66bb45aa5dc8368
Andrey Ilyushchenko <114694451+Andrew-Ilyushchenko@users.noreply.github.com>
2024-02-29 01:50:26 +0300

    feat: add header, without routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,16 @@ import { HttpClientModule } from '@angular/common/http';
 import { GlobalErrorComponent } from './components/global-error/global-error.component';
 import { FormsModule } from '@angular/forms';
 import { FilterProductsPipe } from './pipes/filter-products.pipe';
+import { HeaderComponent } from './components/header/header.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     Product,
     GlobalErrorComponent,
-    FilterProductsPipe
+    FilterProductsPipe,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
